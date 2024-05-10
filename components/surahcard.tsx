72f8155d2f6e62eea7fc938714d87865a23a0e47
Mohammad Faizan <mofaizanfoundations@gmail.com>
2024-05-10 21:30:49 +0530

    Update surahcard.tsx

--- components/surahcard.tsx
+++ components/surahcard.tsx
@@ -9,9 +9,9 @@ interface Props {
 
 const SurahCard = ({ data }: Props) => {
     return (
-        <Link href={`/surah/${data.number}`} className='border-gray-500 border-[0.2px] p-4 py-5 hover:border-[#22A5AD] flex justify-between items-center rounded-md'>
+        <Link href={`/surah/${data.number}`} className='peer border-gray-500 border-[0.2px] p-4 py-5 hover:border-[#22A5AD] flex justify-between items-center rounded-md'>
             <div className='flex gap-2 items-center justify-center'>
-                <span className='flex mr-2 -z-10 bg-gray-800/30 w-10 h-10 rounded-lg text-center items-center justify-center rotate-45'><p className='-rotate-45 text-white font-light'>{data.number}</p></span>
+                <span className='peer-hover:bg-[#22A5AD] flex mr-2 -z-10 bg-gray-800/30 w-10 h-10 rounded-lg text-center items-center justify-center rotate-45'><p className='-rotate-45 text-white font-light'>{data.number}</p></span>
                 <div className='flex flex-col'>
                     <p className='font-semibold text-lg'>{data.englishName}</p>
                     <p className='text-xs text-gray-400'>{data.englishNameTranslation}</p>
